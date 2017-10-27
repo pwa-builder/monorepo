58609c183901e1dd16830d0e856b51c5b6ba7c33
Jeff Burtoft <boyofgreen@gmail.com>
2017-10-27 09:23:23 -0500

    Merge pull request #13 from pwa-builder/revert-12-serviceworkers-update
    Revert "Update service workers code from manifoldjs-lib"

--- packages/pwabuilder-serviceworkers/serviceWorker1/manifoldjs-sw-register.js
+++ packages/pwabuilder-serviceworkers/serviceWorker1/manifoldjs-sw-register.js
@@ -2,10 +2,10 @@
 
 //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 if (navigator.serviceWorker.controller) {
-  console.log('[PWA Builder] active service worker found, no need to register')
+  console.log('[Manifoldjs] active service worker found, no need to register')
 } else {
   //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuider-sw.js', {
+  navigator.serviceWorker.register('manifoldjs-sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);

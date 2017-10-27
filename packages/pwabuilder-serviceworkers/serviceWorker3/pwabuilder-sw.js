fc9ddc40fbc7d584c547af491dd508a9c2ab416e
Jeff Burtoft <boyofgreen@gmail.com>
2017-10-27 10:58:06 -0500

    Merge pull request #14 from southworkscom/serviceworkers-update
    Reapply: Update service workers code from manifoldjs-lib

--- packages/pwabuilder-serviceworkers/serviceWorker3/pwabuilder-sw.js
+++ packages/pwabuilder-serviceworkers/serviceWorker3/pwabuilder-sw.js
@@ -6,9 +6,9 @@ self.addEventListener('install', function(event) {
 });
 
 var preLoad = function(){
-  console.log('[Manifoldjs] Install Event processing');
-  return caches.open('manifoldjs-offline').then(function(cache) {
-    console.log('[Manifoldjs] Cached index and offline page during Install');
+  console.log('[PWA Builder] Install Event processing');
+  return caches.open('pwabuilder-offline').then(function(cache) {
+    console.log('[PWA Builder] Cached index and offline page during Install');
     return cache.addAll(['/offline.html', '/index.html']);
   });
 }
@@ -34,16 +34,16 @@ var checkResponse = function(request){
 };
 
 var addToCache = function(request){
-  return caches.open('manifoldjs-offline').then(function (cache) {
+  return caches.open('pwabuilder-offline').then(function (cache) {
     return fetch(request).then(function (response) {
-      console.log('[manifoldjs] add page to offline'+response.url)
+      console.log('[PWA Builder] add page to offline'+response.url)
       return cache.put(request, response);
     });
   });
 };
 
 var returnFromCache = function(request){
-  return caches.open('manifoldjs-offline').then(function (cache) {
+  return caches.open('pwabuilder-offline').then(function (cache) {
     return cache.match(request).then(function (matching) {
      if(!matching || matching.status == 404) {
        return cache.match('offline.html')

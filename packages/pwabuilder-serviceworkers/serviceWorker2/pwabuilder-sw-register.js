b681e94da9172f4f141e75e668cdeefc6729197c
Jeff Burtoft <boyofgreen@gmail.com>
2017-10-27 09:16:40 -0500

    Merge pull request #12 from southworkscom/serviceworkers-update
    Update service workers code from manifoldjs-lib

--- packages/pwabuilder-serviceworkers/serviceWorker2/pwabuilder-sw-register.js
+++ packages/pwabuilder-serviceworkers/serviceWorker2/pwabuilder-sw-register.js
@@ -1,12 +1,11 @@
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+//This is the "Offline copy of pages" service worker
 
 //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 if (navigator.serviceWorker.controller) {
-  console.log('[Manifoldjs] active service worker found, no need to register')
+  console.log('[PWA Builder] active service worker found, no need to register')
 } else {
-
-//Register the ServiceWorker
-  navigator.serviceWorker.register('manifoldjs-sw.js', {
+  //Register the ServiceWorker
+  navigator.serviceWorker.register('pwabuilder-sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
--- packages/pwabuilder-serviceworkers/serviceWorker3/pwabuilder-sw.js
+++ packages/pwabuilder-serviceworkers/serviceWorker3/pwabuilder-sw.js
@@ -1,14 +1,14 @@
 //This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-//Install stage sets up the offline page in the cahche and opens a new cache
+//Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', function(event) {
   event.waitUntil(preLoad());
 });
 
 var preLoad = function(){
-  console.log('[Manifoldjs] Install Event processing');
-  return caches.open('manifoldjs-offline').then(function(cache) {
-    console.log('[Manifoldjs] Cached index and offline page during Install');
+  console.log('[PWA Builder] Install Event processing');
+  return caches.open('pwabuilder-offline').then(function(cache) {
+    console.log('[PWA Builder] Cached index and offline page during Install');
     return cache.addAll(['/offline.html', '/index.html']);
   });
 }
@@ -34,16 +34,16 @@ var checkResponse = function(request){
 };
 
 var addToCache = function(request){
-  return caches.open('manifoldjs-offline').then(function (cache) {
+  return caches.open('pwabuilder-offline').then(function (cache) {
     return fetch(request).then(function (response) {
-      console.log('[manifoldjs] add page to offline'+response.url)
+      console.log('[PWA Builder] add page to offline'+response.url)
       return cache.put(request, response);
     });
   });
 };
 
 var returnFromCache = function(request){
-  return caches.open('manifoldjs-offline').then(function (cache) {
+  return caches.open('pwabuilder-offline').then(function (cache) {
     return cache.match(request).then(function (matching) {
      if(!matching || matching.status == 404) {
        return cache.match('offline.html')
